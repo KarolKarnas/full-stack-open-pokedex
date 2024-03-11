b40c7854042b0163586225b5ef4bad93426ee4cc
KarolKarnas <karol.karnas@gmail.com>
2024-03-11 11:36:05 +0100

    remove Title

--- src/PokemonList.jsx
+++ src/PokemonList.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Title from './Title'
 
 const PokemonList = ({ pokemonList }) => {
+  // console.log(pokemonList)
   return (
     <div className="list-container">
       {pokemonList.map(({ id, name }) => (
         <Link key={id} to={`/pokemon/${name}`} className="list-item" style={{ backgroundImage: `url(${`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`})` }}>
-          <Title  title={name}/>
+          <div
+            className="list-item-name"
+          >
+            {name}
+          </div>
         </Link>
       ))}
     </div>
